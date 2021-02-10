b7fd9fdff32ad031e0ff1656114cba6d2161078b
Mauro Petrini <dev.mpetrini@gmail.com>
2021-02-09 22:31:55 -0300

    Add test for credentials command with 100% coverage

--- src/credentials.py
+++ src/credentials.py
@@ -1,4 +1,4 @@
-from utils.security import Security
+from src.utils.security import Security
 
 
 class Credentials:
@@ -7,6 +7,12 @@ class Credentials:
         self.secret = None
 
     def add(self, api_key: str, secret: str):
+        if api_key is None or len(api_key) == 0:
+            raise ValueError('api_key cannot be null or empty')
+
+        if secret is None or len(secret) == 0:
+            raise ValueError('secret cannot be null or empty')
+
         self.api_key = api_key
         self.secret = secret
 
@@ -16,5 +22,6 @@ class Credentials:
     def remove(self):
         Security.del_secret_key()
         Security.del_api_key()
+
         self.api_key = None
         self.secret = None
