40f8da8e02f1d10c7ccb6bb8ac5e62d9fac15859
Mauro Petrini <dev.mpetrini@gmail.com>
2021-02-11 23:00:29 -0300

    First mock to spot account_info method

--- tests/test_spot.py
+++ tests/test_spot.py
@@ -1,4 +1,5 @@
 from unittest import TestCase, IsolatedAsyncioTestCase
+from unittest.mock import Mock, patch
 
 from src.spot import Spot
 
@@ -25,8 +26,42 @@ class TestSpotAsync(IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self.spot = Spot()
 
-#    async def test_account_info_return_ok(self):
-#        params = {'recv_window': 5000}
-#        results = await self.spot.account_info(params)
-#
-#        self.assertIsNotNone(results)
+    @patch('src.spot.requests.get')
+    @patch('src.spot.Security.get_api_key_header')
+    @patch('src.spot.Security.get_secret_key')
+    async def test_account_info_return_ok(self, mock_get_secret_key, mock_get_api_key_header, mock_request_get):
+        mock_get_secret_key.return_value = 'SECRET_KEY'
+        mock_get_api_key_header.return_value = {'X-MBX-APIKEY': 'API_KEY'}
+
+        spot_info = {
+            "makerCommission": 15,
+            "takerCommission": 15,
+            "buyerCommission": 0,
+            "sellerCommission": 0,
+            "canTrade": True,
+            "canWithdraw": True,
+            "canDeposit": True,
+            "updateTime": 123456789,
+            "accountType": "SPOT",
+            "balances": [
+                {
+                    "asset": "BTC",
+                    "free": "4723846.89208129",
+                    "locked": "0.00000000"
+                },
+                {
+                    "asset": "ETH",
+                    "free": "0.00000000",
+                    "locked": "0.00000000"
+                }
+            ]
+        }
+
+        mock_request_get.return_value = Mock(status_code=200)
+        mock_request_get.return_value.json.return_value = spot_info
+
+        params = {'recv_window': 5000}
+        results = await self.spot.account_info(**params)
+
+        self.assertIsNotNone(results)
+        self.assertEqual(15, results["makerCommission"])
