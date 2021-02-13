87a8b54fac4a5cb304ea50f548e66076423b559d
Mauro Petrini <dev.mpetrini@gmail.com>
2021-02-12 23:26:22 -0300

    Add spot account_info tests for locked and free balances

--- src/spot.py
+++ src/spot.py
@@ -11,19 +11,6 @@ class Spot:
     def __init__(self):
         pass
 
-    def validate_account_info(self, params):
-        if 'recv_window' not in params:
-            params['recv_window'] = 5_000
-
-        if 'recv_window' in params and int(params['recv_window']) > 60_000:
-            raise ValueError('recv_window cannot exceed 60_000')
-
-        if 'locked_free' in params:
-            locked_free = str(params['locked_free']).upper()
-
-            if locked_free != 'L' or locked_free != 'F' or locked_free != 'B':
-                raise ValueError('locked_free incorrect value. Possible values: L | F | B')
-
     async def account_info(self, **kwargs):
         self.validate_account_info(kwargs)
 
@@ -39,6 +26,32 @@ class Spot:
             return 'Wrong request: status_code: ' + str(r.status_code)
 
         results = r.json()
-        results['balances'] = [x for x in results['balances'] if float(x['free']) > 0.0 or float(x['locked']) > 0.0]
+        results['balances'] = self.filter_balances_account_info(results['balances'], kwargs)
 
         return results
+
+    def filter_balances_account_info(self, balances, filters):
+        if 'locked_free' in filters:
+            locked_free = str(filters['locked_free']).upper()
+
+            if locked_free == 'B':
+                balances = [x for x in balances if float(x['free']) > 0.0 or float(x['locked']) > 0.0]
+            elif locked_free == 'F':
+                balances = [x for x in balances if float(x['free']) > 0.0]
+            elif locked_free == 'L':
+                balances = [x for x in balances if float(x['locked']) > 0.0]
+
+        return balances
+
+    def validate_account_info(self, params):
+        if 'recv_window' not in params:
+            params['recv_window'] = 5_000
+
+        if 'recv_window' in params and int(params['recv_window']) > 60_000:
+            raise ValueError('recv_window cannot exceed 60_000')
+
+        if 'locked_free' in params:
+            locked_free = str(params['locked_free']).upper()
+
+            if locked_free not in ['L', 'F', 'B']:
+                raise ValueError('locked_free incorrect value. Possible values: L | F | B')
--- tests/test_spot.py
+++ tests/test_spot.py
@@ -25,15 +25,7 @@ class TestSpot(TestCase):
 class TestSpotAsync(IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self.spot = Spot()
-
-    @patch('src.spot.requests.get')
-    @patch('src.spot.Security.get_api_key_header')
-    @patch('src.spot.Security.get_secret_key')
-    async def test_account_info_return_ok(self, mock_get_secret_key, mock_get_api_key_header, mock_request_get):
-        mock_get_secret_key.return_value = 'SECRET_KEY'
-        mock_get_api_key_header.return_value = {'X-MBX-APIKEY': 'API_KEY'}
-
-        spot_info = {
+        self.account_info = {
             "makerCommission": 15,
             "takerCommission": 15,
             "buyerCommission": 0,
@@ -53,17 +45,73 @@ class TestSpotAsync(IsolatedAsyncioTestCase):
                     "asset": "ETH",
                     "free": "0.00000000",
                     "locked": "0.00000000"
+                },
+                {
+                    "asset": "BNB",
+                    "free": "0.00000000",
+                    "locked": "10.250"
                 }
             ]
         }
 
-        mock_request_get.return_value = Mock(status_code=200)
-        mock_request_get.return_value.json.return_value = spot_info
+    @patch('src.spot.requests.get')
+    @patch('src.spot.Security.get_api_key_header')
+    @patch('src.spot.Security.get_secret_key')
+    async def test_account_info_all_properties(self, mock_get_secret_key, mock_get_api_key_header, mock_request_get):
+        self.mock_account_info(mock_get_api_key_header, mock_get_secret_key, mock_request_get)
 
         params = {'recv_window': 5000}
         results = await self.spot.account_info(**params)
 
         self.assertIsNotNone(results)
-        self.assertEqual(15, results["makerCommission"])
+        self.assertDictEqual(self.account_info, results)
 
+    @patch('src.spot.requests.get')
+    @patch('src.spot.Security.get_api_key_header')
+    @patch('src.spot.Security.get_secret_key')
+    async def test_account_info_locked_balances(self, mock_get_secret_key, mock_get_api_key_header, mock_request_get):
+        self.mock_account_info(mock_get_api_key_header, mock_get_secret_key, mock_request_get)
 
+        params = {'locked_free': 'L'}
+        results = await self.spot.account_info(**params)
+
+        self.assertIsNotNone(results)
+        self.assertEqual(1, len(results['balances']))
+        self.assertEqual("BNB", results['balances'][0]['asset'])
+        self.assertEqual("0.00000000", results['balances'][0]['free'])
+        self.assertEqual("10.250", results['balances'][0]['locked'])
+
+    @patch('src.spot.requests.get')
+    @patch('src.spot.Security.get_api_key_header')
+    @patch('src.spot.Security.get_secret_key')
+    async def test_account_info_free_balances(self, mock_get_secret_key, mock_get_api_key_header, mock_request_get):
+        self.mock_account_info(mock_get_api_key_header, mock_get_secret_key, mock_request_get)
+
+        params = {'locked_free': 'F'}
+        results = await self.spot.account_info(**params)
+
+        self.assertIsNotNone(results)
+        self.assertEqual(1, len(results['balances']))
+        self.assertEqual("BTC", results['balances'][0]['asset'])
+        self.assertEqual("4723846.89208129", results['balances'][0]['free'])
+        self.assertEqual("0.00000000", results['balances'][0]['locked'])
+
+    @patch('src.spot.requests.get')
+    @patch('src.spot.Security.get_api_key_header')
+    @patch('src.spot.Security.get_secret_key')
+    async def test_account_info_free_and_locked_balances(self, mock_get_secret_key, mock_get_api_key_header, mock_request_get):
+        self.mock_account_info(mock_get_api_key_header, mock_get_secret_key, mock_request_get)
+
+        params = {'locked_free': 'B'}
+        results = await self.spot.account_info(**params)
+
+        self.assertIsNotNone(results)
+        self.assertEqual(2, len(results['balances']))
+        self.assertEqual("BTC", results['balances'][0]['asset'])
+        self.assertEqual("BNB", results['balances'][1]['asset'])
+
+    def mock_account_info(self, mock_get_api_key_header, mock_get_secret_key, mock_request_get):
+        mock_get_secret_key.return_value = 'SECRET_KEY'
+        mock_get_api_key_header.return_value = {'X-MBX-APIKEY': 'API_KEY'}
+        mock_request_get.return_value = Mock(status_code=200)
+        mock_request_get.return_value.json.return_value = self.account_info
