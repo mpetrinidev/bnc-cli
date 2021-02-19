f037b7c3788838c61e3a15ea89700698f1324a46
Mauro Petrini <dev.mpetrini@gmail.com>
2021-02-19 16:54:46 -0300

    Refactor credentials config to config file and add remove credentials

--- src/commands/cmd_credentials.py
+++ src/commands/cmd_credentials.py
@@ -1,8 +1,7 @@
 import click
 
 from src.cli import pass_environment
-from src.utils.security import Security
-from src.utils.utils import write_credentials_config_file
+from src.utils.config import write_credentials, remove_credentials
 
 
 @click.group(short_help="Add or remove Binance CLI credentials (api_key and secret)")
@@ -16,11 +15,7 @@ def cli():
 @pass_environment
 def add(ctx, api_key: str, secret: str):
     """Add Binance CLI's credentials (api_key and secret) to start using Binance CLI"""
-    write_credentials_config_file(api_key, secret)
-
-    Security.set_secret_key(secret)
-    Security.set_api_key(api_key)
-
+    write_credentials(api_key, secret)
     ctx.log("Binance CLI's credentials added successfully")
 
 
@@ -31,8 +26,7 @@ def remove(ctx):
 
     WARNING: You cannot use Binance CLI again until you add new credentials.
     """
-    Security.del_secret_key()
-    Security.del_api_key()
+    remove_credentials()
 
     ctx.log("Binance CLI's credentials removed successfully. \n\nRe-run <bnc credentials add> to start using again "
             "Binance CLI")
--- src/utils/security.py
+++ src/utils/security.py
@@ -3,72 +3,59 @@ import hmac
 import os
 
 from click import ClickException
-from src.utils.utils import read_credentials_config_file
+from src.utils.config import read_credentials
 
 
-class Security:
-    @staticmethod
-    def get_hmac_hash(total_params: str, secret: str) -> str:
-        if len(total_params) == 0:
-            raise ClickException('total_params cannot be empty')
+def get_hmac_hash(total_params: str, secret: str) -> str:
+    if len(total_params) == 0:
+        raise ClickException('total_params cannot be empty')
 
-        if len(secret) == 0:
-            raise ClickException('secret cannot be empty')
+    if len(secret) == 0:
+        raise ClickException('secret cannot be empty')
 
-        signature = hmac.new(str.encode(secret), str.encode(total_params), hashlib.sha256).hexdigest()
-        return signature
+    signature = hmac.new(str.encode(secret), str.encode(total_params), hashlib.sha256).hexdigest()
+    return signature
 
-    @staticmethod
-    def get_api_key_header():
-        return {'X-MBX-APIKEY': Security.get_api_key()}
 
-    @staticmethod
-    def get_api_key():
-        api_key = os.environ.get('BNC_CLI_API_KEY')
+def get_api_key_header():
+    return {'X-MBX-APIKEY': get_api_key()}
 
-        # read from credentials file if it's None
-        if api_key is None:
-            config_values = read_credentials_config_file()
+
+def get_api_key():
+    api_key = os.environ.get('BNC_CLI_API_KEY')
+
+    # read from credentials file if it's None
+    if api_key is None:
+        try:
+            config_values = read_credentials()
             api_key = config_values['api_key']
-
-        if api_key is None:
-            raise ClickException('You must set bnc api_key to start using the CLI')
-
-        return api_key
-
-    @staticmethod
-    def set_api_key(api_key: str):
-        if len(api_key) == 0:
-            raise ClickException('api_key cannot be empty')
-
-        os.environ['BNC_CLI_API_KEY'] = api_key
-
-    @staticmethod
-    def del_api_key():
-        try:
-            del os.environ['BNC_CLI_API_KEY']
         except:
             pass
 
-    @staticmethod
-    def get_secret_key():
-        secret_key = os.environ.get('BNC_CLI_SECRET_KEY')
+    if api_key is None or len(api_key) == 0:
+        raise ClickException('api_key cannot be null or empty')
 
-        if secret_key is None:
-            raise ClickException('You must set bnc secret_key to start using the CLI')
+    return api_key
 
-        return secret_key
 
-    @staticmethod
-    def set_secret_key(secret_key: str):
-        if len(secret_key) == 0:
-            raise ClickException('secret_key cannot be empty')
+def del_api_key():
+    try:
+        del os.environ['BNC_CLI_API_KEY']
+    except:
+        pass
 
-        os.environ['BNC_CLI_SECRET_KEY'] = secret_key
 
-    @staticmethod
-    def del_secret_key():
-        try:
-            del os.environ['BNC_CLI_SECRET_KEY']
-        except:
-            pass
+def get_secret_key():
+    secret_key = os.environ.get('BNC_CLI_SECRET_KEY')
+
+    if secret_key is None:
+        raise ClickException('You must set bnc secret_key to start using the CLI')
+
+    return secret_key
+
+
+def del_secret_key():
+    try:
+        del os.environ['BNC_CLI_SECRET_KEY']
+    except:
+        pass
