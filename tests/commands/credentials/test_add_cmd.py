c7faaf64ee609339989d1ea08d2b07d9479ad528
Mauro Petrini <dev.mpetrini@gmail.com>
2021-04-14 23:43:08 -0300

    Mock dependencies in credentials command tests

--- tests/commands/credentials/test_add_cmd.py
+++ tests/commands/credentials/test_add_cmd.py
@@ -6,7 +6,8 @@ from tests.commands.common_fixtures import *
                                      ['--api_key', 'API_KEY_VALUE', '-s', 'SECRET_VALUE'],
                                      ['-ak', 'API_KEY_VALUE', '--secret', 'SECRET_VALUE'],
                                      ['-ak', 'API_KEY_VALUE', '-s', 'SECRET_VALUE']])
-def test_credentials_add_options_successfully(runner, options):
+def test_credentials_add_options_successfully(runner, options, mocker):
+    mocker.patch('src.commands.cmd_credentials.write_credentials', return_value=None)
     result = runner.invoke(add, options)
     assert result.exit_code == 0
     assert result.output == "Binance CLI's credentials added successfully\n"
--- tests/commands/credentials/test_show_cmd.py
+++ tests/commands/credentials/test_show_cmd.py
@@ -1,15 +1,12 @@
 from tests.commands.common_fixtures import *
 from src.commands.cmd_credentials import show
-from src.utils.config import write_credentials
-from src.utils.config import remove_credentials
 
 
-def test_credentials_show_successfully(runner):
-    write_credentials("API_KEY_VALUE", "SECRET_KEY_VALUE")
+def test_credentials_show_successfully(runner, mocker):
+    mocker.patch('src.commands.cmd_credentials.get_api_key', return_value="API_KEY_VALUE")
+    mocker.patch('src.commands.cmd_credentials.get_secret_key', return_value="SECRET_KEY_VALUE")
 
     result = runner.invoke(show)
 
     assert result.exit_code == 0
     assert result.output == "BNC_CLI_API_KEY: API_KEY_VALUE\nBNC_CLI_SECRET_KEY: SECRET_KEY_VALUE\n"
-
-    remove_credentials()
