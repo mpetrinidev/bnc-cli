df3bb4f1aec7962eeb8062a3aabc02a16b1ca3c2
Mauro Petrini <dev.mpetrini@gmail.com>
2021-04-18 23:41:28 -0300

    Add more details and fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,58 @@
+import pathlib
+
 from setuptools import setup
 
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
 setup(
-    name="bnc",
-    version="1.0.0",
-    packages=["src", "src.commands"],
-    include_package_data=True,
-    install_requires=["click"],
+    name='bnc',  # Required
+    version='0.0.1-alpha',  # Required
+    description='Unofficial Binance CLI to interact with Binance API',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/mpetrinidev/bnc-cli',  # Optional
+    author='Mauro Petrini',  # Optional
+    author_email='dev.mpetrini@gmail.com',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        "Intended Audience :: Binance's Traders",
+        'Topic :: Software Development :: Command Line Interface',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    keywords='bnc, binance, binance-api, binance-cli, bnc-cli',  # Optional
+    packages=['src', 'src.commands', 'src.utils', 'src.validation'],  # Required
+    python_requires='>=3.8, <4',
+    install_requires=[
+        'requests_async~=0.6.2',
+        'click~=7.1.2',
+        'pandas~=1.2.2',
+        'PyYAML~=5.4.1',
+        'jmespath~=0.10.0'
+    ],  # Optional
     entry_points="""
         [console_scripts]
         bnc=src.cli:cli
     """,
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/mpetrinidev/bnc-cli/issues',
+        'Source': 'https://github.com/mpetrinidev/bnc-cli',
+    },
 )
