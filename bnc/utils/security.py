0030c9758a6edb0ed4f1dfa5a5ff1ac26e2b91e4
Mauro Petrini <dev.mpetrini@gmail.com>
2021-06-02 23:36:14 -0300

    Add new tests for testnet environment variables

--- bnc/utils/security.py
+++ bnc/utils/security.py
@@ -19,9 +19,13 @@ def get_hmac_hash(total_params: str, secret: str) -> str:
 
 
 def get_api_key():
-    api_key = os.environ.get('BNC_CLI_API_KEY')
+    config = read_configuration()
 
-    # Check env variable
+    api_key = os.environ.get('BNC_TESTNET_CLI_API_KEY') \
+        if config['is_testnet'] \
+        else os.environ.get('BNC_CLI_API_KEY')
+
+    # Check env variabletest_get_api_key_from_env_ok
     if api_key is None:
         try:
             config_values = read_credentials()
@@ -31,8 +35,7 @@ def get_api_key():
 
     # Check config file variable
     if api_key is None or len(api_key) == 0:
-        config = read_configuration()
-        raise SecurityException(f'Credentials are required. Run: {"bnc" if config["is_testnet"] else "bnc_testnet"} '
+        raise SecurityException(f'Credentials are required. Run: {"bnc_testnet" if config["is_testnet"] else "bnc"} ' 
                                 'credentials add --api_key="your_api_key" '
                                 '--secret="your_secret_key" to start using the CLI or add credentials using '
                                 'env variables')
@@ -41,7 +44,11 @@ def get_api_key():
 
 
 def get_secret_key():
-    secret_key = os.environ.get('BNC_CLI_SECRET_KEY')
+    config = read_configuration()
+
+    secret_key = os.environ.get('BNC_TESTNET_CLI_SECRET_KEY') \
+        if config['is_testnet'] \
+        else os.environ.get('BNC_CLI_SECRET_KEY')
 
     # Check env variable
     if secret_key is None:
@@ -53,7 +60,8 @@ def get_secret_key():
 
     # Check config file variable
     if secret_key is None or len(secret_key) == 0:
-        raise SecurityException('Credentials are required. Run: bnc credentials add --api_key="your_api_key" '
+        raise SecurityException(f'Credentials are required. Run: {"bnc_testnet" if config["is_testnet"] else "bnc"} '
+                                'credentials add --api_key="your_api_key" '
                                 '--secret="your_secret_key" to start using the CLI or add credentials using '
                                 'env variables')
 
--- tests/utils/test_security.py
+++ tests/utils/test_security.py
@@ -15,8 +15,11 @@ API_KEY_HEADER = {'X-MBX-APIKEY': API_KEY}
 
 
 @pytest.fixture()
-def mocked_bnc_config_path(mocker):
+def mocked_defaults(mocker):
     mocker.patch('bnc.utils.config.get_bnc_config_path', return_value=get_bnc_test_config_path())
+    mocker.patch('bnc.utils.security.read_configuration', return_value={'is_testnet': False})
+
+    return mocker
 
 
 def get_bnc_test_config_path():
@@ -40,59 +43,69 @@ def test_get_hmac_hash_secret_new_signature():
     assert get_hmac_hash(total_params, secret) == '32d322a281bcd36c64af2bc97e13eee974f24ac900fe4dc7b4901f166d72e6cc'
 
 
-def test_get_api_key_from_env_ok():
-    os.environ['BNC_CLI_API_KEY'] = API_KEY
+@pytest.mark.parametrize("key,testnet", [('BNC_CLI_API_KEY', False), ('BNC_TESTNET_CLI_API_KEY', True)])
+def test_get_api_key_from_env_ok(mocker, key, testnet):
+    mocker.patch('bnc.utils.security.read_configuration', return_value={'is_testnet': testnet})
+    os.environ[key] = API_KEY
     assert get_api_key() == API_KEY
-    del os.environ['BNC_CLI_API_KEY']
+    del os.environ[key]
 
 
-def test_get_api_key_from_config_file(mocked_bnc_config_path):
+def test_get_api_key_from_config_file(mocked_defaults):
     write_credentials_file(API_KEY, SECRET_KEY)
     assert get_api_key() == API_KEY
     remove_credentials()
 
 
-def test_get_api_key_null_or_empty(mocker):
-    mocker.patch('os.environ.get', return_value=None)
-    mocker.patch('bnc.utils.security.read_credentials', side_effect=ConfigException('Custom_Exception'))
+@pytest.mark.parametrize('command_text,testnet', [('bnc', False), ('bnc_testnet', True)])
+def test_get_api_key_null_or_empty(mocked_defaults, command_text, testnet):
+    mocked_defaults.patch('os.environ.get', return_value=None)
+    mocked_defaults.patch('bnc.utils.security.read_credentials', side_effect=ConfigException('Custom_Exception'))
+    mocked_defaults.patch('bnc.utils.security.read_configuration', return_value={'is_testnet': testnet})
 
     with pytest.raises(SecurityException,
-                       match='Credentials are required. Run: bnc credentials add --api_key="your_api_key" '
+                       match=f'Credentials are required. Run: {command_text} credentials add --api_key="your_api_key" '
                              '--secret="your_secret_key" to start using the CLI or add credentials using '
                              'env variables'):
         get_api_key()
 
 
-def test_get_secret_from_env_ok():
-    os.environ['BNC_CLI_SECRET_KEY'] = SECRET_KEY
+@pytest.mark.parametrize('key,testnet', [('BNC_CLI_SECRET_KEY', False), ('BNC_TESTNET_CLI_SECRET_KEY', True)])
+def test_get_secret_from_env_ok(mocker, key, testnet):
+    mocker.patch('bnc.utils.security.read_configuration', return_value={'is_testnet': testnet})
+    os.environ[key] = SECRET_KEY
     assert get_secret_key() == SECRET_KEY
-    del os.environ['BNC_CLI_SECRET_KEY']
+    del os.environ[key]
 
 
-def test_get_secret_from_config_file(mocked_bnc_config_path):
+def test_get_secret_from_config_file(mocked_defaults):
     write_credentials_file(API_KEY, SECRET_KEY)
     assert get_secret_key() == SECRET_KEY
     remove_credentials()
 
 
-def test_get_secret_null_or_empty(mocker):
-    mocker.patch('os.environ.get', return_value=None)
-    mocker.patch('bnc.utils.security.read_credentials', side_effect=ConfigException('Custom_Exception'))
+@pytest.mark.parametrize('command_text,testnet', [('bnc', False), ('bnc_testnet', True)])
+def test_get_secret_null_or_empty(mocked_defaults, command_text, testnet):
+    mocked_defaults.patch('os.environ.get', return_value=None)
+    mocked_defaults.patch('bnc.utils.security.read_credentials', side_effect=ConfigException('Custom_Exception'))
+    mocked_defaults.patch('bnc.utils.security.read_configuration', return_value={'is_testnet': testnet})
 
-    with pytest.raises(SecurityException, match='Credentials are required. Run: bnc credentials add '
+    with pytest.raises(SecurityException, match=f'Credentials are required. Run: {command_text} credentials add '
                                                 '--api_key="your_api_key" '
                                                 '--secret="your_secret_key" to start using the CLI or add credentials '
                                                 'using env variables'):
         get_secret_key()
 
 
-def test_get_api_key_header_from_env_ok():
-    os.environ['BNC_CLI_API_KEY'] = API_KEY
+@pytest.mark.parametrize("key,testnet", [('BNC_CLI_API_KEY', False), ('BNC_TESTNET_CLI_API_KEY', True)])
+def test_get_api_key_header_from_env_ok(mocker, key, testnet):
+    mocker.patch('bnc.utils.security.read_configuration', return_value={'is_testnet': testnet})
+    os.environ[key] = API_KEY
     assert get_api_key_header() == API_KEY_HEADER
-    del os.environ['BNC_CLI_API_KEY']
+    del os.environ[key]
 
 
-def test_get_api_key_header_from_config_file_ok(mocked_bnc_config_path):
+def test_get_api_key_header_from_config_file_ok(mocked_defaults):
     write_credentials_file(API_KEY, SECRET_KEY)
     assert get_api_key_header() == API_KEY_HEADER
     remove_credentials()
